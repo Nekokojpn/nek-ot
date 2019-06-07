3c2ea92b1725524ea20a89a90ac23d64e9bc4b85
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-06-08 02:47:17 +0900

    com

--- toyLang.cpp
+++ toyLang.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+
+// IFDEF
+#define HIGH_DEBUGG
 
 // GLOBALS------------->
 std::string source;
-char cc;        // Current char.
+int cc;        // Current char.
 std::string cs; // Current string EX. identiflier,number.
 int pc = 0;		//Program counter
 int line = 0;	//source line
+std::vector<int> tokens;
 
 // TOKENS--------------->
 enum {
@@ -14,13 +23,19 @@ enum {
 
   tok_int = 100,
 
-  tok_num = 200
+  tok_num = 200,
+
+  tok_eof = -1
 };
 
 //Useful funcs----------->
+
+void error(std::string message) { std::cerr << message << std::endl; }
+void error_at(std::string message) {}
+
 void get_char() { cc = source[pc++]; }
 //Add to cs
-void add_char() { cs += cc; }
+void add_char() { cs += std::to_string(cc); }
 //Compare
 bool compare_cs(const char* str) { return cs == str; };
 
@@ -28,6 +43,9 @@ bool compare_cs(const char* str) { return cs == str; };
 // LEXER----------------->
 int gettoken() {
   get_char();
+  #ifdef HIGH_DEBUGG
+    std::cout<<"cc="<<cc<<std::endl;
+  #endif
   // skip any spaces.
   while (isspace(cc)) {
     get_char();
@@ -35,11 +53,17 @@ int gettoken() {
 
   // if source[pc] is alpha char.
   if (isalpha(cc)) { // Regex, [A-Z]|[a-z]+[digit]*
+  std::cout<<"alph!!";
     cs = cc;
-
-    while (!isspace(cc) || isalnum(cc)) {
-      add_char();
-	}
+    get_char();
+    while (isalnum(cc)){
+        get_char();
+        add_char();
+    }
+    #ifdef HIGH_DEBUGG
+    std::cout<<"cs="<<cc<<std::endl;
+    #endif
+    if(cs=="fn")std::cout<<"OK";
     if (compare_cs("fn")) {
       return tok_fn;
 	} else if (compare_cs("int")) {
@@ -47,13 +71,32 @@ int gettoken() {
 	}
   } else if (isdigit(cc)) {
 
-  }
+  }else if(cc == EOF)return tok_eof;
   error("Unknown token '" + std::to_string(cc) + "', Exit.");
+  return 1;
 }
 
-void error(std::string message) { std::cerr << message << std::endl; }
-void error_at(std::string message) {
-  
+//Load source
+int load_source(){
+    std::ifstream ifs("./source_test.nk");
+    if(ifs.fail()){error("Failed to open source file.");return 1;}
+    std::string buf;
+    while(getline(ifs,buf))
+    {
+        source+=buf;
+    }
+    std::cout<<source<<std::endl;
+    return 0;
 }
 
-int main() {}
+int main() {
+    if(load_source()==1)return 1;
+    int tok = gettoken();
+    while(tok !=tok_eof){
+        tokens.push_back(tok);
+        tok = gettoken();
+        int n;
+        std::cin>>n;
+    }
+    for(auto i : tokens)std::cout<<i<<std::endl;
+}

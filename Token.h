ff2c694c3589b8910a11b224ba144608513a418c
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-06-16 05:29:20 +0900

    impl ret

--- Token.h
+++ Token.h
@@ -4,8 +4,9 @@
 enum {
   tok_fn = 2,
 
-  tok_void = 100,
-  tok_int = 101,
+  tok_ret = 100,
+  tok_void = 101,
+  tok_int = 102,
 
   tok_num_int = 200,
   tok_num_double = 201,
@@ -16,9 +17,13 @@ enum {
   tok_rp = 303, // )
   tok_lb = 304, // {
   tok_rb = 305, // }
-  tok_minus = 306,
-  tok_plus = 307,
-  tok_arrow = 308,
+  tok_arrow = 306,
+
+  // operator
+  tok_plus = 400,
+  tok_minus = 401,
+  tok_star = 402,
+  tok_slash = 403,
 
   tok_identifier = 500,
 

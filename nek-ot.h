1685965004a4d754e7371ed8afeaedb97c2e5bd3
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-06-26 01:33:22 +0900

    Implement main header file

---
+++ nek-ot.h
@@ -0,0 +1,86 @@
+#pragma once
+
+//#define HIGH_DEBUGG			//ハイ・レベルデバッグ
+#define DEBUGG						//デバッグモード.
+
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
+#include <windows.h>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <stack>
+
+//		Tokens----->
+// TOKENS--------------->
+enum class TK {
+	tok_nope = 0,
+	tok_fn = 2,
+
+	tok_ret = 100,
+	tok_void = 101,
+	tok_int = 102,
+	tok_float = 103,
+	tok_double = 104,
+	tok_short = 105,
+	tok_long = 106,
+	tok_char = 107,
+	tok_string = 108,
+
+	tok_num_int = 200,
+	tok_num_double = 201,
+	tok_str_string = 202,
+
+	tok_semi = 300,
+	tok_equal = 301,
+	tok_lp = 302, // (
+	tok_rp = 303, // )
+	tok_lb = 304, // {
+	tok_rb = 305, // }
+	tok_arrow = 306, // ->
+	tok_sq = 307, // '
+	tok_dq = 308, // "
+
+	// operator
+	tok_plus = 400,
+	tok_minus = 401,
+	tok_star = 402,
+	tok_slash = 403,
+
+	tok_identifier = 500,
+
+	tok_eof = -1,
+	tok_unknown = 0
+
+};
+
+// typedef----->
+typedef struct {
+	TK ty;
+	std::string val;
+} Token_t;
+//<-----
+
+class Console {
+public:
+	static void SetConsoleTextGray();
+	static void SetConsoleTextRed();
+	static void SetConsoleTextGreen();
+	static void SetConsoleTextWhite();
+	static void SetConsoleTextBlue();
+};

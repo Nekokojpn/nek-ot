0eb242d9b6447283f0f20384751810e91f89c4c3
R1ku2 <riku8798@gmail.com>
2019-06-30 23:11:29 +0900

    Improve codes

--- Parser.cpp
+++ Parser.cpp
@@ -1,5 +1,41 @@
 #include "nek-ot.h"
 
+extern LLVMContext TheContext;
+extern IRBuilder<> Builder(TheContext);
+extern std::unique_ptr<Module> TheModule;
+
+std::map<std::string, Value*> NamedValues;
+
+Value* ASTValue::codegen() {
+	Value* value = NamedValues[name];
+	if (!value)
+	{
+	}
+	return value;
+}
+
+
+Value* ASTBinOp::codegen() {
+	Value* l = lhs->codegen();
+	Value* r = lhs->codegen();
+	if (!l || !r)
+		return nullptr;
+	switch (op) {
+	case Op::Plus:
+		return Builder.CreateFAdd(l, r, "addtmp");
+	case Op::Minus:
+		return Builder.CreateFSub(l, r, "subtmp");
+	case Op::Mul:
+		return Builder.CreateFMul(l, r, "multmp");
+	case Op::Div:
+		l = Builder.CreateFCmpULT(l, r, "cmptmp");
+		return Builder.CreateUIToFP(l, Type::getDoubleTy(TheContext), "booltmp");
+	default:
+		return nullptr;
+	}
+}
+
+
 bool Parser::consume(TK tk) {
 	if (tokens[index].ty == tk)
 	{
@@ -53,13 +89,12 @@ std::unique_ptr<AST> Parser::expr_mul() {
 std::unique_ptr<AST> Parser::expr_primary() {
 	Token_t t = get();
 	if (t.ty != TK::tok_num_int) {
-		error("Unko", "unexpected");
+		error("Unexpected", "must be a number.");
 		exit(1);
 	}
 	auto value = std::make_unique<ASTValue>(std::atoi(t.val.c_str()));
 	return std::move(value);
 }
-
 
 Parser::Parser(std::vector<Token_t> _tokens) : tokens(_tokens) {
 	index = 0;

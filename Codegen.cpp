1e3b12d69ce5129bba833475dbaa058fa3c089d7
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-07-02 02:52:56 +0900

    Implement intCodegen

--- Codegen.cpp
+++ Codegen.cpp
@@ -1,1 +1,32 @@
 #include "nek-ot.h"
+
+
+Value* ASTValue::codegen(IRBuilder<>& Builder) {
+	return Builder.getInt32(value);
+}
+
+Value* ASTBinOp::codegen(IRBuilder<>& Builder) {
+	Value* l = lhs->codegen(Builder);
+	Value* r = rhs->codegen(Builder);
+	if (!l || !r)
+		return nullptr;
+	switch (op) {
+	case Op::Plus:
+		return Builder.CreateAdd(l, r, "addtmp");
+	case Op::Minus:
+		return Builder.CreateSub(l, r, "subtmp");
+	case Op::Mul:
+		return Builder.CreateMul(l, r, "multmp");
+	case Op::Div:
+		return Builder.CreateSDiv(l, r, "divtmp");
+	default:
+		return nullptr;
+	}
+}
+
+Value* ASTInt::codegen(IRBuilder<>& Builder) {
+	Value* value = expr_p->codegen(Builder);
+	if (!value)
+		return nullptr;
+	return value;
+}

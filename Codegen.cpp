8bbbfa8812c0118d27d0920ef542dc69e8592e3b
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-07-13 21:06:57 +0900

    Improve codegen

--- Codegen.cpp
+++ Codegen.cpp
@@ -1,9 +1,51 @@
+#pragma once
 #include "nek-ot.h"
 
+LLVMContext context;
+IRBuilder<> builder(context);
+std::unique_ptr<Module> module;
+
+static std::map<std::string, FunctionCallee> functions_global;
+static std::map<std::string, Value*> namedvalues_global;
+static std::map<std::string, Value*> namedvalues_local;
+
+std::map<std::string, Value*>&  getNamedValues_Local() {
+	return namedvalues_local;
+}
+
+void Sys::IO::CreateFunc() {
+	//puts --ïWèÄèoóÕ
+	std::vector<Type*> putsArgs;
+	putsArgs.push_back(builder.getInt8Ty()->getPointerTo());
+	ArrayRef<Type*>  argsRef(putsArgs);
+	FunctionType* putsType =
+		FunctionType::get(builder.getInt32Ty(), argsRef, false);
+	FunctionCallee putsFunc = module->getOrInsertFunction("puts", putsType);
+	functions_global["puts"] = putsFunc;
+}
+
+/*
 LLVMContext& context = getContext();
 IRBuilder<>& builder = getBuilder();
 Module* module = getModule();
-std::map<std::string, Value*>& namedvalueslocal = getNamedValues_Local();
+*/
+
+//Internal Library Classes----->
+
+
+//<-----
+
+
+Function* curfunc;
+BasicBlock* curbb;
+
+
+void Parser::init_parse() {
+	module = make_unique<Module>("top", context);
+}
+void Parser::dump() {
+	module->dump();
+}
 
 Value* ASTIdentifier::codegen() {
 	return value;
@@ -38,17 +80,17 @@ Value* ASTBoolOp::codegen() {
 		return nullptr;
 	switch (bop) {
 	case BOp::LThan:
-		return builder.CreateFCmpONE(l, r, "iftemp");
+		return builder.CreateICmpSLT(l, r, "iftemp");
 	case BOp::LThanEqual:
-		return builder.CreateAdd(l, r, "addtmp");
+		return builder.CreateICmpSLE(l, r, "addtmp");
 	case BOp::RThan:
-		return builder.CreateAdd(l, r, "addtmp");
+		return builder.CreateICmpSGT(l, r, "addtmp");
 	case BOp::RThanEqual:
-		return builder.CreateAdd(l, r, "addtmp");
+		return builder.CreateICmpSGE(l, r, "addtmp");
 	case BOp::EqualEqual:
-		return builder.CreateAdd(l, r, "addtmp");
+		return builder.CreateICmpEQ(l, r, "addtmp");
 	case BOp::NotEqual:
-		return builder.CreateAdd(l, r, "addtmp");
+		return builder.CreateICmpNE(l, r, "addtmp");
 	default:
 		return nullptr;
 	}
@@ -56,22 +98,54 @@ Value* ASTBoolOp::codegen() {
 
 Value* ASTInt::codegen() {
 	auto value = expr_p->codegen();
-	namedvalueslocal[name] = value;
+	
 	if (!value)
 		return nullptr;
+	namedvalues_local[name] = value;
+	builder.CreateAlloca(builder.getInt32Ty(), value, name);
 	return value;
 }
-
-Value* ASTFunc::codegen() {
+Value* ASTProto::codegen() {
+	Function* mainFunc =
+		Function::Create(FunctionType::get(Type::getVoidTy(context), false),
+			Function::ExternalLinkage, name, module.get());
+	curfunc = mainFunc;
+	if (name == "main") {
+		curbb = BasicBlock::Create(context, "entry", mainFunc);
+		builder.SetInsertPoint(curbb);
+	}
+	else {
+		curbb = BasicBlock::Create(context, "", mainFunc);
+		builder.SetInsertPoint(curbb);
+	}
 	return nullptr;
 }
+Value* ASTFunc::codegen() {
+	auto pr = proto->codegen();
+	for (int i = 0; i < body.size(); i++) {
+		body[i]->codegen();
+	}
+	return pr;
+}
 Value* ASTArgProto::codegen() {
+	return nullptr;
+}
+Value* ASTElse::codegen() {
 	return nullptr;
 }
 
 Value* ASTIf::codegen() {
 	auto astboolop = proto->codegen();
+	if (!astboolop)
+		return nullptr;
 	//TODO define if elif BasicBlock 
-	auto ast = body->codegen();
-	return nullptr;
+
+	BasicBlock* if_block = BasicBlock::Create(context,"if_block");
+	BasicBlock* cont = BasicBlock::Create(context, "cont");
+	if(ast_elif == nullptr && ast_else == nullptr) // ifÇµÇ©ë∂ç›ÇµÇ»Ç¢èÍçá.
+		auto branch = builder.CreateCondBr(astboolop, if_block, cont);
+	for (int i = 0; i < body.size(); i++) {
+		body[i]->codegen();
+	}
+	return astboolop;
 }

02f72586ea9e1518c29c2f85bb8acca94a903d86
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-07-28 00:59:53 +0900

    Add ts test case file

--- test_sources/TESTCASE.ts
+++ test_sources/TESTCASE.ts
@@ -1,3 +1,14 @@
 cd current
-echo "function definition"
-3024=>%f("def_func.nk")
+echo "int_mul_definition_test"
+assert 10 => %f "def_int.nk"
+echo "function_definition_test"
+assert 840 => %f "def_func.nk"
+echo "hello_world_test"
+assert "Hello_nek-ot!" => %f "helloworld.nk" -stdout
+echo "if_test"
+assert 4 => %f "if.nk"
+echo "while_test"
+assert 1000000000 => %f "while.nk"
+echo "fibonacci_test"
+assert 0 => %f "fibo.nk"
+end

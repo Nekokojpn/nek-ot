9a82caab3fda357d3b4df03c81f494cbc2219fbc
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-07-28 08:12:45 +0900

    file dest expr

---
+++ expr.cpp
@@ -0,0 +1,74 @@
+#include "nek-ot.h"
+
+std::unique_ptr<AST> Parser::expr() {
+
+	std::unique_ptr<AST> ast = expr_add();
+	return ast;
+}
+std::unique_ptr<AST> Parser::expr_add() {
+	auto lhs = expr_mul();
+	while (true) {
+		Op op;
+		if (consume(TK::tok_plus)) {
+			op = Op::Plus;
+		}
+		else if (consume(TK::tok_minus)) {
+			op = Op::Minus;
+		}
+		else {
+			break;
+		}
+		auto rhs = expr_mul();
+		lhs = std::make_unique<ASTBinOp>(std::move(lhs), op, std::move(rhs));
+	}
+	return std::move(lhs);
+}
+std::unique_ptr<AST> Parser::expr_mul() {
+	auto lhs = expr_primary();
+	while (true) {
+		Op op;
+		if (consume(TK::tok_star)) {
+			op = Op::Mul;
+		}
+		else if (consume(TK::tok_slash)) {
+			op = Op::Div;
+		}
+		else {
+			break;
+		}
+		auto rhs = expr_primary();
+		lhs = std::make_unique<ASTBinOp>(std::move(lhs), op, std::move(rhs));
+	}
+	return std::move(lhs);
+}
+std::unique_ptr<AST> Parser::expr_primary() {
+
+	if (curtok.ty == TK::tok_num_int) {
+		auto value = std::make_unique<ASTValue>(std::atoi(curtok.val.c_str()));
+		getNextToken(); //eat num
+		return std::move(value);
+	}
+	else if (curtok.ty == TK::tok_identifier) {
+		auto identifier = std::make_unique<ASTIdentifier>(curtok.val);
+		getNextToken();
+		if (curtok.ty == TK::tok_lp) { //Function call.
+			auto funccall = func_call(identifier->name);
+			return std::move(funccall);
+		}
+		return std::move(identifier);
+	}
+	else if (curtok.ty == TK::tok_lp) {
+		getNextToken();
+		auto ast = expr();
+		if (curtok.ty == TK::tok_rp)
+		{
+			getNextToken();
+			return std::move(ast);
+		}
+
+		error("Expected", "Expected --> )", curtok);
+	}
+	error("Unexpected", "must be a number.", curtok);
+	exit(1);
+
+}

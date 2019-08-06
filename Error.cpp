67ab5912cf4264fbf4d609d47800fa3454d303d1
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-08-06 14:50:24 +0900

    Implment error overload func

--- Error.cpp
+++ Error.cpp
@@ -38,18 +38,18 @@ void error(std::string title, std::string message, int line, int column) {
 	std::cerr << "  |";
 	exit(1);
 }
-void error(std::string title, std::string message, Token_t& curtok) {
+void error(std::string title, std::string message, Location_t& loc) {
 	std::cerr << std::endl;
 	Console::SetConsoleTextRed();
 	std::cerr << "error: " << title << std::endl;
 	Console::SetConsoleTextBlue();
 	std::cerr << " --> ";
 	Console::SetConsoleTextWhite();
-	std::cerr << source_filename << ":" << curtok.loc.location_begin_line + 1 << ":" << curtok.loc.location_begin_column << std::endl; //"~" << curtok.loc.location_end_line << ":" << curtok.loc.location_end_column << std::endl;
+	std::cerr << source_filename << ":" << loc.location_begin_line + 1 << ":" << loc.location_begin_column << std::endl; //"~" << loc.location_end_line << ":" << loc.location_end_column << std::endl;
 	Console::SetConsoleTextBlue();
-	std::cerr << "  |" << std::endl << curtok.loc.location_begin_line+1 << " |";
+	std::cerr << "  |" << std::endl << loc.location_begin_line+1 << " |";
 	Console::SetConsoleTextWhite();
-	std::string t = source[curtok.loc.location_begin_line].substr(0, source[curtok.loc.location_begin_line].size() - 1);
+	std::string t = source[loc.location_begin_line].substr(0, source[loc.location_begin_line].size() - 1);
 	int i = 0;
 	while (isspace(t[i++]));
 	t = t.substr(i - 1, t.size() - 1);
@@ -58,11 +58,11 @@ void error(std::string title, std::string message, Token_t& curtok) {
 	std::cerr << "  |";
 	Console::SetConsoleTextWhite();
 	std::cerr << "     ";
-	for (int i = 0; i < curtok.loc.location_begin_column - 1; i++)
+	for (int i = 0; i < loc.location_begin_column - 1; i++)
 		std::cerr << " ";
 	Console::SetConsoleTextRed();
-	if (curtok.loc.location_begin_column <= curtok.loc.location_end_column) {
-		for (int i = curtok.loc.location_begin_column; i <= curtok.loc.location_end_column; i++) {
+	if (loc.location_begin_column <= loc.location_end_column) {
+		for (int i = loc.location_begin_column; i <= loc.location_end_column; i++) {
 			std::cerr << "^" << std::endl;
 		}
 	}
@@ -72,7 +72,7 @@ void error(std::string title, std::string message, Token_t& curtok) {
 	Console::SetConsoleTextBlue();
 	std::cerr << "  |";
 	Console::SetConsoleTextWhite();
-	for (int i = 0; i < curtok.loc.location_begin_column - 1; i++)
+	for (int i = 0; i < loc.location_begin_column - 1; i++)
 		std::cerr << " ";
 	Console::SetConsoleTextRed();
 	std::cerr << message << std::endl;
@@ -80,6 +80,10 @@ void error(std::string title, std::string message, Token_t& curtok) {
 	std::cerr << "  |";
 	exit(1);
 }
+void error(std::string title, std::string message, Token_t& curtok) {
+	error(title, message, curtok.loc);
+}
+
 void error_unexpected(Token_t& curtok) {
 	error("Unexpected", "Unexpected token --> " + curtok.val, curtok);
 }

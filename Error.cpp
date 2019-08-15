b75fa5f4cd2c848aaf97bca770370e26ad96fce4
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-08-16 03:36:59 +0900

    Improve error visual

--- Error.cpp
+++ Error.cpp
@@ -14,14 +14,17 @@ void error(std::string title, std::string message, int line, int column) {
 	Console::SetConsoleTextBlue();
 	std::cerr << "  |" << std::endl << line + 1 << " |";
 	Console::SetConsoleTextWhite();
-	std::string t = source[line].substr(0, source[line].size() - 1);
+	std::vector<std::string> t;
+	if (line - 1 >= 0)
+		t.push_back(source[line - 1].substr(0, source[line - 1].size() - 1));
+	t.push_back(source[line].substr(0, source[line].size() - 1));
 	int i = 0;
-	while (isspace(t[i++]));
-	t = t.substr(i - 1, t.size() - 1);
-	std::cerr << "     " << t << std::endl;
-	Console::SetConsoleTextBlue();
-	std::cerr << "  |";
-	Console::SetConsoleTextWhite();
+	for (std::string s : t) {
+		std::cerr << "     " << s << std::endl;
+		Console::SetConsoleTextBlue();
+		std::cerr << "  |";
+		Console::SetConsoleTextWhite();
+	}
 	std::cerr << "     ";
 	for (int i = 0; i < column - 1; i++)
 		std::cerr << " ";
@@ -39,51 +42,12 @@ void error(std::string title, std::string message, int line, int column) {
 	exit(1);
 }
 void error(std::string title, std::string message, Location_t& loc) {
-	std::cerr << std::endl;
-	Console::SetConsoleTextRed();
-	std::cerr << "error: " << title << std::endl;
-	Console::SetConsoleTextBlue();
-	std::cerr << " --> ";
-	Console::SetConsoleTextWhite();
-	std::cerr << source_filename << ":" << loc.location_begin_line + 1 << ":" << loc.location_begin_column << std::endl; //"~" << loc.location_end_line << ":" << loc.location_end_column << std::endl;
-	Console::SetConsoleTextBlue();
-	std::cerr << "  |" << std::endl << loc.location_begin_line+1 << " |";
-	Console::SetConsoleTextWhite();
-	std::string t = source[loc.location_begin_line].substr(0, source[loc.location_begin_line].size() - 1);
-	int i = 0;
-	while (isspace(t[i++]));
-	t = t.substr(i - 1, t.size() - 1);
-	std::cerr << "     " << t << std::endl;
-	Console::SetConsoleTextBlue();
-	std::cerr << "  |";
-	Console::SetConsoleTextWhite();
-	std::cerr << "     ";
-	for (int i = 0; i < loc.location_begin_column - 1; i++)
-		std::cerr << " ";
-	Console::SetConsoleTextRed();
-	if (loc.location_begin_column <= loc.location_end_column) {
-		for (int i = loc.location_begin_column; i <= loc.location_end_column; i++) {
-			std::cerr << "^" << std::endl;
-		}
-	}
-	else {
-		std::cerr << "^" << std::endl;
-	}
-	Console::SetConsoleTextBlue();
-	std::cerr << "  |";
-	Console::SetConsoleTextWhite();
-	for (int i = 0; i < loc.location_begin_column - 1; i++)
-		std::cerr << " ";
-	Console::SetConsoleTextRed();
-	std::cerr << message << std::endl;
-	Console::SetConsoleTextBlue();
-	std::cerr << "  |";
-	exit(1);
+	error(title, message, loc.location_begin_line, loc.location_begin_column);
 }
 void error(std::string title, std::string message, Token_t& curtok) {
 	error(title, message, curtok.loc);
 }
 
 void error_unexpected(Token_t& curtok) {
-	error("Unexpected", "Unexpected token --> " + curtok.val, curtok);
+	error("Compile error", "Unexpected token --> " + curtok.val, curtok);
 }

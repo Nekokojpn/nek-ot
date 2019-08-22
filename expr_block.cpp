a9721227c77ab42d18c4368b61dfc96a3336b652
R1ku2 <riku8798@gmail.com>
2019-08-22 09:09:59 +0900

    Improve st

--- expr_block.cpp
+++ expr_block.cpp
@@ -41,7 +41,15 @@ std::vector<std::unique_ptr<AST>> Parser::expr_block() { //  {expr block}
 			auto ast = def_ret();
 			asts.push_back(std::move(ast));
 		}
+		else if (curtok.ty == TK::tok_action) {
+			auto ast = def_action();
+			asts.push_back(std::move(ast));
+		}
 		else getNextToken();
 	}
+	getNextToken();
+	if (curtok.ty != TK::tok_semi)
+		error("Expected", "Expected --> ;", curtok);
+	getNextToken();
 	return asts;
 }

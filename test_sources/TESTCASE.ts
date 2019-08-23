e910c8dd5f0e729e5da4975e8c44c3e86a6185a3
R1ku2 <riku8798@gmail.com>
2019-08-23 13:38:57 +0900

    Update test

--- test_sources/TESTCASE.ts
+++ test_sources/TESTCASE.ts
@@ -1,14 +1,8 @@
 cd current
-echo "int_mul_definition_test"
-assert 10 => %f "def_int.nk"
-echo "function_definition_test"
-assert 840 => %f "def_func.nk"
-echo "hello_world_test"
-assert "Hello_nek-ot!" => %f "helloworld.nk" -stdout
-echo "if_test"
-assert 4 => %f "if.nk"
-echo "while_test"
-assert 1000000000 => %f "while.nk"
-echo "fibonacci_test"
-assert 0 => %f "fibo.nk"
+echo "i32_test1"
+assert 1 => %f "i32_test1.nk" -stdout
+echo "i32_test2"
+assert 100 => %f "i32_test2.nk" -stdout
+echo "fibo35_test"
+assert 9227465 => %f "fibo35.nk" -stdout
 end

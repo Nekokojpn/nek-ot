9767406398fc64903b9b9eca31a27a1cd530b0fb
R1ku2 <riku8798@gmail.com>
2019-08-23 13:39:16 +0900

    Improve header

--- nek-ot.h
+++ nek-ot.h
@@ -35,6 +35,7 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Support/raw_os_ostream.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 #include <windows.h>				//Using at Console class
 #include <cctype>
@@ -71,12 +72,8 @@ enum class TK {
 	tok_i64,
 	tok_char,
 	tok_string,
-	tok_i32_arr,
 	tok_action,
 	tok_var,
-	tok_char_arr,
-	tok_float_arr,
-	tok_double_arr,
 
 	tok_num_int,
 	tok_num_double,
@@ -105,6 +102,8 @@ enum class TK {
 	tok_rpb, // ]
 	tok_percent, // %
 	tok_darrow, // =>
+	tok_colon,
+	tok_cleq,
 
 	// operator
 	tok_plus,
@@ -283,8 +282,12 @@ class ASTType : public AST { // i32 i = 0;
 public:
 	AType ty;
 	std::string name;
+	/* FOR ARRAY ATTRIBUTES*/
+	long long arr_size;
+	//
 	std::unique_ptr<ASTSubst> expr;
 	ASTType(AType _ty, std::string _name, std::unique_ptr<ASTSubst> _expr) : ty(_ty), name(_name), expr(std::move(_expr)) {};
+	ASTType(AType _ty, std::string _name, long long size) : ty(_ty), name(_name) {};
 	Value* codegen() override;
 };
 
@@ -292,7 +295,7 @@ class ASTArrType : public AST {
 public:
 	AArrType ty;
 	std::string name;
-	long size;
+	long long size;
 	ASTArrType(AArrType _ty, std::string _name, long _size) : ty(_ty), name(_name), size(_size) {};
 	Value* codegen() override;
 };
@@ -408,8 +411,7 @@ class Parser {
 
 	std::unique_ptr<ASTStrLiteral> expr_str();
 
-	std::unique_ptr<ASTType> def_type();
-	std::unique_ptr<ASTArrType> def_arr_type();
+	std::unique_ptr<ASTType> def_type(const std::string& _id);
 	std::unique_ptr<ASTString> def_string();
 	std::unique_ptr<ASTFunc> def_func();
 	std::unique_ptr<ASTAction> def_action();
@@ -435,5 +437,4 @@ public:
 	void setOpt(bool b);
 	bool getOpt();
 	AType getATypeByCurtok();
-	AArrType getAArrTypeByCurtok();
 };

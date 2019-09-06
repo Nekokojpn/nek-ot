dbc1168ac8581c5e7b04fd6827a22afef4bbab37
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-09-07 03:29:21 +0900

    Folder change

---
+++ Parser/expr.cpp
@@ -0,0 +1,109 @@
+#include "nek-ot.h"
+
+std::unique_ptr<AST> Parser::expr() {
+
+	std::unique_ptr<AST> ast = expr_add();
+	return ast;
+}
+std::unique_ptr<AST> Parser::expr_add() {
+	auto lhs = expr_mul();
+	while (true) {
+		Op op;
+		if (consume(TK::tok_plus)) {
+			op = Op::Plus;
+		}
+		else if (consume(TK::tok_minus)) {
+			op = Op::Minus;
+		}
+		else {
+			break;
+		}
+		auto rhs = expr_mul();
+		auto loc = curtok.loc;
+		lhs = std::make_unique<ASTBinOp>(std::move(lhs), op, std::move(rhs));
+		lhs->loc = loc;
+	}
+	return std::move(lhs);
+}
+std::unique_ptr<AST> Parser::expr_mul() {
+	auto lhs = expr_primary();
+	while (true) {
+		Op op;
+		if (consume(TK::tok_star)) {
+			op = Op::Mul;
+		}
+		else if (consume(TK::tok_slash)) {
+			op = Op::Div;
+		}
+		else if (consume(TK::tok_percent)) {
+			op = Op::RDiv;
+		}
+		else {
+			break;
+		}
+		auto rhs = expr_primary();
+		auto loc = curtok.loc;
+		lhs = std::make_unique<ASTBinOp>(std::move(lhs), op, std::move(rhs));
+		lhs->loc = loc;
+	}
+	return std::move(lhs);
+}
+std::unique_ptr<AST> Parser::expr_primary() {
+
+	if (curtok.ty == TK::tok_num_int) {
+		auto loc = curtok.loc;
+		auto value = std::make_unique<ASTValue>(std::atoll(curtok.val.c_str()));
+		value->loc = loc;
+		getNextToken(); //eat num
+		return std::move(value);
+	}
+	else if (curtok.ty == TK::tok_dq) {
+		auto loc = curtok.loc;
+		auto ast = std::make_unique<ASTString>("", std::move(expr_str()));
+		ast->loc = loc;
+		return std::move(ast);
+	}
+	else if (curtok.ty == TK::tok_plpl || curtok.ty == TK::tok_mimi) {
+		
+	}
+	else if (curtok.ty == TK::tok_identifier) {
+		auto loc = curtok.loc;
+		auto identifier = std::make_unique<ASTIdentifier>(curtok.val);
+		identifier->loc = loc;
+		getNextToken();
+		if (curtok.ty == TK::tok_lp) { //Function call.
+			auto funccall = func_call(identifier->name);
+			return std::move(funccall);
+		}
+		else if (curtok.ty == TK::tok_lpb) { //Array
+			std::vector<std::unique_ptr<AST>> expr_v;
+			while (curtok.ty == TK::tok_lpb) { //Array
+				getNextToken();
+				expr_v.push_back(std::move(expr()));
+				if (curtok.ty != TK::tok_rpb)
+					error_unexpected(curtok);
+				getNextToken();
+			}
+			auto loc = curtok.loc;
+			auto ast = std::make_unique<ASTIdentifierArrayElement>(identifier->name, std::move(expr_v));
+			ast->loc = loc;
+			return std::move(ast);
+		}
+		return std::move(identifier);
+	}
+	else if (curtok.ty == TK::tok_lp) {
+		getNextToken();
+		auto ast = expr();
+		ast->loc = curtok.loc;
+		if (curtok.ty == TK::tok_rp)
+		{
+			getNextToken();
+			return std::move(ast);
+		}
+
+		error("Expected", "Expected --> )", curtok);
+	}
+	error_unexpected(curtok);
+	exit(1);
+
+}

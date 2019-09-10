29fe8450fb34a65cc7cc2805f82a57a2430507f6
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-09-11 02:30:45 +0900

    Implement struct parse

--- Parser/struct.cpp
+++ Parser/struct.cpp
@@ -1,16 +1,40 @@
 #include "../nek-ot.h"
-std::unique_ptr<AST> expr_stct() {
-	return nullptr;
-}
 
 std::unique_ptr<AST> Parser::def_stct() {
+	getNextToken();
+	if (curtok.ty != TK::tok_identifier) {
+		add_err_msg(curtok.val + " may be used as a reserved word.");
+		error_unexpected(curtok);
+	}
+	auto name = curtok.val;
 	getNextToken();
 	if (curtok.ty != TK::tok_lb) {
 		error_unexpected(curtok);
 	}
 	getNextToken();
-	while (curtok.ty != TK::tok_rb) {
 
+	std::vector<AType> putsArgs;
+	std::vector<std::string> argsIdentifier;
+	while (true) {
+		AType ty = getATypeByCurtok();
+		if (ty == AType::Nop)break;
+
+		putsArgs.push_back(ty);
+
+		getNextToken();
+		if (curtok.ty != TK::tok_identifier)
+			error_unexpected(curtok);
+		argsIdentifier.push_back(curtok.val);
+		getNextToken();
+		if (curtok.ty != TK::tok_comma) break;
+		getNextToken();
 	}
-	return nullptr;
+
+	if (curtok.ty != TK::tok_rp)
+		error("Expected", "Expected --> )", curtok);
+	getNextToken();
+	if (curtok.ty != TK::tok_semi)
+		error("Expected", "Expected --> )", curtok);
+	getNextToken();
+	return std::make_unique<ASTStruct>(name, putsArgs);
 }

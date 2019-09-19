f9d8d8f60b9d37b40d9d7587c55b0801991f3f77
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-09-20 06:22:31 +0900

    Impl experimental expr dot function.

---
+++ Parser/expr_dot.cpp
@@ -0,0 +1,12 @@
+#include "../nek-ot.h"
+
+//TODO experimental function for stct accessor.
+std::unique_ptr<AST> Parser::expr_dot(std::string& identifier) {
+	getNextToken(); //eat .
+	if (curtok.ty != TK::tok_identifier) {
+		error_unexpected(curtok);
+	}
+	auto id = curtok.val;
+	getNextToken();
+	return nullptr;
+}

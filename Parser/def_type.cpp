ce00531983a65a743f6faf13f8ccd3fff11f1944
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-09-24 05:25:58 +0900

    Improve codes

--- Parser/def_type.cpp
+++ Parser/def_type.cpp
@@ -1,9 +1,10 @@
 #include "../nek-ot.h"
 
 std::unique_ptr<ASTType> Parser::def_type(const std::string& _id) {
+	bool isonlydef = curtok.ty == TK::tok_colon ? true : false;
 	getNextToken();
 	auto ty = getATypeByCurtok(); //TODO: Support stct;
-	std::string stct_name = "";
+	std::string stct_name;
 	if (ty == AType::Struct)
 		stct_name = curtok.val;
 	if (ty != AType::Nop) {
@@ -33,35 +34,70 @@ std::unique_ptr<ASTType> Parser::def_type(const std::string& _id) {
 					error_unexpected(curtok);
 				getNextToken();
 			}
-			if (curtok.ty != TK::tok_lp) {
-				error_expected("(", curtok);
-			}
-			getNextToken();
-			if (curtok.ty != TK::tok_rp) {
-				error_expected(")", curtok);
-			}
-			getNextToken();
-			if (curtok.ty == TK::tok_lb) { // The array declaration has a body.
-				auto loc = curtok.loc;
-				auto ast = std::make_unique<ASTType>(ty, _id, size_v, std::move(expr_arr()), stct_name);
-				ast->loc = loc;
-				return std::move(ast);
-			}
-			else if (curtok.ty == TK::tok_semi) { // The array declaration has no body.
+			if (!isonlydef) {
+				if (curtok.ty != TK::tok_lp) {
+					error_expected("(", curtok);
+				}
 				getNextToken();
-				auto loc = curtok.loc;
-				auto ast = std::make_unique<ASTType>(ty, _id, size_v, nullptr, stct_name);
-				ast->loc = loc;
-				return std::move(ast);
+				if (curtok.ty != TK::tok_rp) {
+					error_expected(")", curtok);
+				}
+				getNextToken();
+				if (curtok.ty == TK::tok_lb) { // The array declaration has a body.
+					auto loc = curtok.loc;
+					auto ast = std::make_unique<ASTType>(ty, _id, size_v, std::move(expr_arr()), stct_name);
+					ast->loc = loc;
+					return std::move(ast);
+				}
+				else if (curtok.ty == TK::tok_semi) { // The array declaration has no body.
+					getNextToken();
+					auto loc = curtok.loc;
+					auto ast = std::make_unique<ASTType>(ty, _id, size_v, nullptr, stct_name);
+					ast->loc = loc;
+					return std::move(ast);
+				}
+			}
+			else {
+				if (curtok.ty == TK::tok_semi) {
+					auto loc = curtok.loc;
+					auto ast = std::make_unique<ASTType>(ty, _id, size_v, nullptr, stct_name);
+					ast->loc = loc;
+					return std::move(ast);
+				}
 			}
 			getNextToken();
 		}
-		else {
-			if (curtok.ty != TK::tok_lp)
-				error_unexpected(curtok);
-			getNextToken();
-			if (curtok.ty == TK::tok_rp) {
+		else { // for var control
+			if (!isonlydef) {
+				bool doll = false;
+				if (curtok.ty != TK::tok_lp &&
+					curtok.ty != TK::tok_doll)
+					error_unexpected(curtok);
+				if (curtok.ty == TK::tok_doll)doll = true;
 				getNextToken();
+				if (curtok.ty == TK::tok_rp) {
+					getNextToken();
+					if (curtok.ty != TK::tok_semi)
+						error_expected(";", curtok);
+					getNextToken();
+					auto loc = curtok.loc;
+					auto ast = std::make_unique<ASTType>(ty, _id, nullptr, stct_name);
+					ast->loc = loc;
+					return std::move(ast);
+				}
+				auto loc = curtok.loc;
+				auto ast = std::make_unique<ASTType>(ty, _id, std::make_unique<ASTSubst>(std::make_unique<ASTIdentifier>(_id), std::move(expr())), stct_name);
+				ast->loc = loc;
+				if (!doll && curtok.ty != TK::tok_rp)
+					error_expected(")", curtok);
+				if (!doll)
+					getNextToken();
+				if (curtok.ty != TK::tok_semi)
+					error_expected(";", curtok);
+				getNextToken();
+				return std::move(ast);
+			}
+			else {
 				if (curtok.ty != TK::tok_semi)
 					error_expected(";", curtok);
 				getNextToken();
@@ -70,16 +106,6 @@ std::unique_ptr<ASTType> Parser::def_type(const std::string& _id) {
 				ast->loc = loc;
 				return std::move(ast);
 			}
-			auto loc = curtok.loc;
-			auto ast = std::make_unique<ASTType>(ty, _id, std::make_unique<ASTSubst>(std::make_unique<ASTIdentifier>(_id), std::move(expr())), stct_name);
-			ast->loc = loc;
-			if (curtok.ty != TK::tok_rp)
-				error_expected(")", curtok);
-			getNextToken();
-			if (curtok.ty != TK::tok_semi)
-				error_expected(";", curtok);
-			getNextToken();
-			return std::move(ast);
 		}
 	}
 	auto loc = curtok.loc;

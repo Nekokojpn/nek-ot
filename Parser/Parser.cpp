9b8004c6c6f1c847fbf6dd47472a2aa6f4529dfc
R1ku2 <riku8798@gmail.com>
2019-10-03 10:18:28 +0900

    Support break;

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -329,42 +329,6 @@ std::unique_ptr<ASTIf> Parser::bool_statement() {
 	return nullptr;
 }
 
-std::unique_ptr<AST> Parser::bool_expr() {
-	auto lhs = expr();
-	while (true) {
-		Op op;
-		if (consume(TK::tok_lt)) {
-			op = Op::LThan;
-		}
-		else if (consume(TK::tok_lteq)) {
-			op = Op::LThanEqual;
-		}
-		else if (consume(TK::tok_rt)) {
-			op = Op::RThan;
-		}
-		else if (consume(TK::tok_rteq)) {
-			op = Op::RThanEqual;
-		}
-		else if (consume(TK::tok_eqeq)) {
-			op = Op::EqualEqual;
-		}
-		else if(consume(TK::tok_emeq)) {
-			op = Op::NotEqual;
-		}
-		else {
-			break;
-		}
-		auto rhs = expr();
-		if (curtok.ty == TK::tok_ampamp) {
-			getNextToken();
-			//TODO Support &&
-		}
-		auto loc = curtok.loc;
-		lhs = std::make_unique<ASTBinOp>(std::move(lhs), op, std::move(rhs));
-		lhs->loc = loc;
-	}
-	return std::move(lhs);
-}
 
 std::unique_ptr<ASTFor> Parser::for_statement() {
 	getNextToken();

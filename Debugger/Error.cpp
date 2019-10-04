408d80583b7feaa2f70b20a27c2e38f86b22110f
R1ku2 <riku8798@gmail.com>
2019-10-04 14:52:34 +0900

    Improve error showing

--- Debugger/Error.cpp
+++ Debugger/Error.cpp
@@ -24,11 +24,11 @@ void error(std::string title, std::string message, uint32_t line, uint32_t colum
 	Console::SetConsoleTextRed();
 	std::cerr << "Error: " << title << std::endl;
 	Console::SetConsoleTextBlue();
-	std::cerr << " --> ";
+	std::cerr << "  --> ";
 	Console::SetConsoleTextWhite();
 	std::cerr << source_filename << ":" << line + 1 << ":" << column << std::endl;
 	Console::SetConsoleTextBlue();
-	std::cerr << line - 1 << " |" << std::endl << line << " |";
+	printf("%5d|\n%5d|", line - 1, line);
 	Console::SetConsoleTextWhite();
 	std::vector<std::string> t;
 	if (line - 1 >= 0)
@@ -39,16 +39,20 @@ void error(std::string title, std::string message, uint32_t line, uint32_t colum
 	for (std::string s : t) {
 		std::cerr << "     " << s << std::endl;
 		Console::SetConsoleTextBlue();
-		std::cerr << line + ll++ << " |";
+		printf("%5d|", line + ll++);
 		Console::SetConsoleTextWhite();
 	}
 	Console::SetConsoleTextWhite();
 	for (int i = 0; i < column - 1; i++)
 		std::cerr << " ";
 	Console::SetConsoleTextRed();
+	std::cerr << "     ^" << std::endl;
+	Console::SetConsoleTextBlue();
+	printf("%5d|", line + ll);
+	Console::SetConsoleTextRed();
 	std::cerr << message << std::endl;
 	Console::SetConsoleTextBlue();
-	std::cerr << line + ll << " |" << std::endl;
+	printf("%5d|", line + ll);
 	Console::SetConsoleTextRed();
 	std::cerr << errmsg << std::endl;
 	Console::SetConsoleTextGray();

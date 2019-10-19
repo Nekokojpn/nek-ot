eab3e9a3f317ada75f636495bf9f94cf3efa4713
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2019-10-19 17:47:55 +0900

    Add stct controls

--- Parser/expr_subst.cpp
+++ Parser/expr_subst.cpp
@@ -5,7 +5,9 @@ std::unique_ptr<ASTSubst> Parser::subst_expr(const std::string& _id, TypeKind ty
 	auto id = std::make_unique<ASTIdentifier>(_id, ty_kind);
 	id->loc = loc;
 	std::unique_ptr<ASTIdentifierArrayElement> id2;
-	if (curtok.ty == TK::tok_lpb) { //Array
+	std::unique_ptr<AST> id_stct;
+	//Array------------------->
+	if (curtok.ty == TK::tok_lpb) { 
 		std::vector<std::unique_ptr<AST>> expr_v;
 		while (curtok.ty == TK::tok_lpb) { 
 			getNextToken();
@@ -18,6 +20,15 @@ std::unique_ptr<ASTSubst> Parser::subst_expr(const std::string& _id, TypeKind ty
 		id2 = std::make_unique<ASTIdentifierArrayElement>(_id, std::move(expr_v), ty_kind);
 		id2->loc = loc;
 	}
+	//<--------------------Array
+	//Stct-------------------->
+	if (curtok.ty == TK::tok_dot) {
+		auto loc = curtok.loc;
+		id_stct = expr_dot(id->name);
+		id_stct->loc = loc;
+	}
+	//<--------------------Stct
+	//Subst------------------->
 	if (curtok.ty == TK::tok_equal) {
 		if (!id2) {
 			getNextToken();
@@ -28,7 +39,7 @@ std::unique_ptr<ASTSubst> Parser::subst_expr(const std::string& _id, TypeKind ty
 				error_unexpected(curtok);
 			return std::move(ast);
 		}
-		else { // ArrayType
+		else if(id2) { // ArrayType
 			getNextToken();
 			auto loc = curtok.loc;
 			auto ast = std::make_unique<ASTSubst>(std::move(id2), std::move(expr()));
@@ -38,7 +49,19 @@ std::unique_ptr<ASTSubst> Parser::subst_expr(const std::string& _id, TypeKind ty
 			getNextToken();
 			return std::move(ast);
 		}
+		else { //Stct
+			getNextToken();
+			auto loc = curtok.loc;
+			auto ast = std::make_unique<ASTSubst>(std::move(id_stct), std::move(expr()));
+			ast->loc = loc;
+			if (curtok.ty != TK::tok_semi)
+				error_unexpected(curtok);
+			getNextToken();
+			return std::move(ast);
+		}
 	}
+	//<--------------------Subst
+	//Lambda------------------->
 	else if (curtok.ty == TK::tok_darrow) {
 		getNextToken();
 		auto loc = curtok.loc;
@@ -50,6 +73,7 @@ std::unique_ptr<ASTSubst> Parser::subst_expr(const std::string& _id, TypeKind ty
 		getNextToken();
 		return std::move(ast);
 	}
+	//<-------------------Lambda
 	else if (curtok.ty == TK::tok_plpl || curtok.ty == TK::tok_mimi) {
 		auto loc = curtok.loc;
 		auto ast = std::make_unique<ASTSubst>(std::move(id), std::move(expr()));
@@ -59,9 +83,11 @@ std::unique_ptr<ASTSubst> Parser::subst_expr(const std::string& _id, TypeKind ty
 		getNextToken();
 		return std::move(ast);
 	}
+	//do nothing--------------->
 	else if (curtok.ty == TK::tok_semi) {
 		return nullptr;
 	}
+	//<----------------do nothing
 	else
 		error("Unexpected", "Unexpected token -->" + curtok.val, curtok);
 

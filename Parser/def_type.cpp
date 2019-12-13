0d6031d326fad3ad1a23102db5e303221f53b024
R1ku2 <riku8798@gmail.com>
2019-12-13 12:00:54 +0900

    Refactoring

--- Parser/def_type.cpp
+++ Parser/def_type.cpp
@@ -1,55 +1,11 @@
 #include "../nek-ot.hpp"
 
 ASTType* Parser::def_type(AST* ast_id) {
-	bool isonlydef = curtok.ty == TK::tok_colon ? true : false;
 	getNextToken();
 	std::string stct_name = curtok.val;
 	auto ty = getTypeFromCurtok();
 	ASTSubst* expr_ = nullptr;
-	if (!isonlydef) {
-		if (curtok.ty == TK::tok_semi) {
-			getNextToken();
-			auto loc = curtok.loc;
-			auto ast = new ASTType(ty,
-				ast_id,
-				expr_,
-				stct_name,
-				this->cdgen->IsGlobal()
-				);
-			ast->name = this->curval;
-			ast->loc = loc;
-			return ast;
-		}
-		auto loc = curtok.loc;
-		ASTType* ast = nullptr;
-		//Arr
-		if (curtokIs(TK::tok_lb)) {
-			ast = new ASTType(ty,
-				ast_id,
-				expr(),
-				stct_name,
-				this->cdgen->IsGlobal()
-			);
-		}
-		else {
-			ast = new ASTType(ty,
-				ast_id,
-				new ASTSubst(ast_id, expr()),
-				stct_name,
-				this->cdgen->IsGlobal()
-			);
-		}
-		ast->name = this->curval;
-		ast->loc = loc;
-		if (this->isExpectedSemi && curtok.ty != TK::tok_semi)
-			error_expected(";", curtok);
-		if(this->isExpectedSemi)getNextToken();
-		this->isExpectedSemi = true;
-		return ast;
-	}
-	else {
-		if (curtok.ty != TK::tok_semi)
-			error_expected(";", curtok);
+	if (curtok.ty == TK::tok_semi) {
 		getNextToken();
 		auto loc = curtok.loc;
 		auto ast = new ASTType(ty,
@@ -63,6 +19,32 @@ ASTType* Parser::def_type(AST* ast_id) {
 		return ast;
 	}
 	auto loc = curtok.loc;
+	ASTType* ast = nullptr;
+	//Arr
+	if (curtokIs(TK::tok_lb)) {
+		ast = new ASTType(ty,
+			ast_id,
+			expr(),
+			stct_name,
+			this->cdgen->IsGlobal()
+		);
+	}
+	else {
+		ast = new ASTType(ty,
+			ast_id,
+			new ASTSubst(ast_id, expr()),
+			stct_name,
+			this->cdgen->IsGlobal()
+		);
+	}
+	ast->name = this->curval;
+	ast->loc = loc;
+	if (this->isExpectedSemi && curtok.ty != TK::tok_semi)
+		error_expected(";", curtok);
+	if(this->isExpectedSemi)getNextToken();
+	this->isExpectedSemi = true;
+	return ast;
+	loc = curtok.loc;
 	error_unexpected(curtok);
 	return nullptr;
 }

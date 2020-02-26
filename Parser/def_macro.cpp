d30e5ec5b68cad617f50c5eceabf2456dcf5f4c5
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2020-02-26 23:07:43 +0900

    Fix bug

--- Parser/def_macro.cpp
+++ Parser/def_macro.cpp
@@ -14,7 +14,7 @@ void Parser::def_macro() {
 				error_expected("identifier", curtok);
 			args.push_back(curtok);
 			getNextToken();
-			if (!curtokIs(TK::tok_colon))
+			if (!curtokIs(TK::tok_comma))
 				break;
 			getNextToken();
 		}
@@ -38,26 +38,70 @@ void Parser::def_macro() {
 			auto next = tokens[i+1];
 			//if exists argument
 			if (next.ty == TK::tok_lp) {
-				auto cnt = 2;
-				next = tokens[i + cnt++];
-				
+				tokens.erase(tokens.begin() + i + 1);
+				std::vector<Token_t> real_args;
+				while (true) {
+					next = tokens[i + 1];
+					real_args.push_back(next);
+					tokens.erase(tokens.begin() + i + 1);
+					next = tokens[i + 1];
+					tokens.erase(tokens.begin() + i + 1);
+					if (next.ty == TK::tok_comma)
+						continue;
+					else if (next.ty == TK::tok_rp)
+						break;
+					else
+						error_unexpected(next);
+				}
+				bool overrideflg = false;
+				for (int j = 0; j < mac_lit.size(); j++) {
+					auto& tkn = mac_lit[j];
+					if (tkn.ty == TK::tok_identifier) {
+						
+						for (int k = 0; k < args.size(); k++) {
+							auto a = args[k];
+							if (a.val == tkn.val) {
+								if (overrideflg) {
+									tokens.insert(tokens.begin() + i + j, real_args[k]);
+								}
+								else {
+									overrideflg = true;
+									tokens[i+j] = real_args[k];
+								}
+							}
+							else {
+								if (overrideflg) {
+									tokens.insert(tokens.begin() + i + j, tkn);
+								}
+								else {
+									overrideflg = true;
+									tokens[i + j] = tkn;
+								}
+							}
+						}
+					}
+					else {
+						if (overrideflg) {
+							tokens.insert(tokens.begin() + i + j, tkn);
+						}
+						else {
+							overrideflg = true;
+							tokens[i+j] = tkn;
+						}
+					}	
+				}
 			}
 			else {
-				for (auto& tkn : mac_lit) {
-					/*if (tkn.ty == TK::tok_identifier) {
-						for (auto& c : args) {
-							if (c.val == tkn.val) {
-								//tokens[i++] =
-							}
-							else
-								tokens[i++] = tkn;
-						}
-					}*/
-					tokens[i] = tkn;
+				tokens[i] = mac_lit[0];
+				for (int j = 1; j < mac_lit.size(); j++) {
+					tokens.insert(tokens.begin() + i + j, mac_lit[j]);
 				}
+
 			}
 			t.val = mac_name;
 		}
 	}
+	for (auto& t : tokens)
+		std::cout << t.val << std::endl;
 	return;
 }

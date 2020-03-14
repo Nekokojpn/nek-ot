2ad60dce67a313b5927818b42cd9ae4373747eb9
Nekokojpn <43212800+Nekokojpn@users.noreply.github.com>
2020-03-14 09:36:04 +0900

    Delete some tests & Fix array bug

--- Codegen/IdentifierBase.cpp
+++ Codegen/IdentifierBase.cpp
@@ -8,19 +8,19 @@ Value* ASTIdentifierBase::codegen() {
 	else
 		current_inst_load = current_inst;
 	*/
-	auto value = Codegen::getLocalVal(name, this->loc);
-	if (!value)
-		return Codegen::getGlobalVal(name, this->loc);
+	auto value = Codegen::getDefinedValue(name, this->loc);
+	auto ty_load = value->getType()->isPointerTy() ? value->getType()->getPointerElementType() : value->getType();
 	if (isSubst || namedvalues_local_isinitialized[this->name] == true) {
 		current_inst = value;
 		if (isSubst)namedvalues_local_isinitialized[this->name] = true;
-		return value;
 	}
 	else {
 		add_err_msg("Variables must be initialized before use.");
 		error_codegen("Variable '" + this->name + "' is not initialized!", this->loc);
 	}
-	
+	if (!ty_load->isStructTy()) {
+		//TODO
+	}
 }
 
 Type* ASTIdentifierBase::getType() {
